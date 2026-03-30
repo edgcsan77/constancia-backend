729ebc4d99184844c23a52a582f9d6ff777a0547
edgcsan77 <edgcsan@gmail.com>
2026-03-30 13:06:35 -0500

    Update rfc.py

--- rfc.py
+++ rfc.py
@@ -1328,7 +1328,7 @@ USERS = {
     #"eos":generate_password_hash("EOScif26"),
     "omar.perez":generate_password_hash("PerezCIF26"),
     "omar.perez2":generate_password_hash("PerezCIF26"),
-    #"brandon.user":generate_password_hash("BrandonCIF26"),
+    "brandon.user":generate_password_hash("BrandonCIF26"),
     "alejandro.user":generate_password_hash("AlejandroIDCIF26"),
     "alejandro.user2":generate_password_hash("AlejandroIDCIF26"),
     "mariano.gonzalez":generate_password_hash("MarianoIDCIF26"),
